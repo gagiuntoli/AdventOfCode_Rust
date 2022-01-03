0a2f179c16a3ec78d190f7f91423cec69509a697
Guido Giuntoli <gagiuntoli@gmail.com>
2022-01-03 00:07:54 -0300

    Improving day 1 2021

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -3,7 +3,6 @@ use std::io::{self, BufRead};
 use std::env;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
     let file = File::open(args[1].clone()).unwrap();
     let values = io::BufReader::new(file)
@@ -11,26 +10,11 @@ fn main() {
         .map(|line| line.unwrap().parse().unwrap())
         .collect::<Vec<u32>>();
 
-    let mut count1 = 0;
-    for i in 1..values.len() {
-        if values[i] > values[i-1] {
-            count1 += 1;
-        }
-    }
+    let count1 = values.iter().enumerate().filter(|&(i,_)| i != 0 && values[i] > values[i-1]).count();
     assert!(count1 == 1583);
     println!("part 1 = {}", count1);
 
-    let mut count2 = 0;
-    for i in 1..values.len()-2 {
-        if values[i+2] > values[i-1] {
-            count2 += 1;
-        }
-    }
+    let count2 = values.iter().enumerate().filter(|&(i,_)| i != 0 && i < values.len()-2 && values[i+2] > values[i-1]).count();
     assert!(count2 == 1627);
     println!("part 2 = {}", count2);
 }
-
-/*
-part 1 = 1583
-part 2 = 1627
-*/
