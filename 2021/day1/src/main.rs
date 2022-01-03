8702b41b6485a71d58aff47b1667603564ac8e96
Guido Giuntoli <gagiuntoli@gmail.com>
2022-01-02 23:57:34 -0300

    Improving day 1 & 2 2021

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -6,14 +6,10 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     let file = File::open(args[1].clone()).unwrap();
-
-    let mut values : Vec<u32> = Vec::new();
-    let lines = io::BufReader::new(file).lines();
-    for line in lines {
-        if let Ok(ip) = line {
-            values.push(ip.parse::<u32>().unwrap());
-        }
-    }
+    let values = io::BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect::<Vec<u32>>();
 
     let mut count1 = 0;
     for i in 1..values.len() {
@@ -21,12 +17,20 @@ fn main() {
             count1 += 1;
         }
     }
+    assert!(count1 == 1583);
+    println!("part 1 = {}", count1);
+
     let mut count2 = 0;
     for i in 1..values.len()-2 {
         if values[i+2] > values[i-1] {
             count2 += 1;
         }
     }
-    println!("part 1 = {}", count1);
+    assert!(count2 == 1627);
     println!("part 2 = {}", count2);
 }
+
+/*
+part 1 = 1583
+part 2 = 1627
+*/
--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -2,9 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::env;
 
-struct Instruction {
-    direction : String,
-    value: u64,
+struct Inst {
+    dir: String,
+    val: u64,
 }
 
 fn main() {
@@ -12,40 +12,39 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(args[1].clone()).unwrap();
 
-    let mut values : Vec<Instruction> = Vec::new();
-    let lines = io::BufReader::new(file).lines();
-    for line in lines {
-        if let Ok(ip) = line {
-            let instruction : Vec<&str> = ip.split(' ').collect();
-            values.push(Instruction {
-                direction: instruction[0].to_string(),
-                value: instruction[1].parse::<u64>().unwrap()
-            });
-        }
-    }
+    let values = io::BufReader::new(file)
+        .lines()
+        .map(|line| {
+            match line.unwrap().split_once(' ').unwrap() {
+                (a,b) => Inst {dir: a.to_string(), val: b.parse().unwrap()}
+            }
+        })
+        .collect::<Vec<Inst>>();
 
     let mut horizontal = 0;
     let mut vertical = 0;
     for v in &values {
-        match v.direction.as_ref() {
-            "forward" => horizontal += v.value,
-            "up" => vertical -= v.value,
-            "down" => vertical += v.value,
-            _ => println!("Direction not recognized: {}", v.direction)
+        match v.dir.as_ref() {
+            "forward" => horizontal += v.val,
+            "up" => vertical -= v.val,
+            "down" => vertical += v.val,
+            _ => unreachable!(),
         } 
     }
+    assert!(horizontal * vertical == 2039256);
     println!("part 1 = {}", horizontal * vertical);
 
     let mut aim = 0;
     let mut horizontal = 0;
     let mut vertical = 0;
     for v in &values {
-        match v.direction.as_ref() {
-            "forward" => {horizontal += v.value; vertical += aim * v.value},
-            "up" => aim -= v.value,
-            "down" => aim += v.value,
-            _ => println!("Direction not recognized: {}", v.direction)
+        match v.dir.as_ref() {
+            "forward" => {horizontal += v.val; vertical += aim * v.val},
+            "up" => aim -= v.val,
+            "down" => aim += v.val,
+            _ => unreachable!()
         } 
     }
+    assert!(horizontal * vertical == 1856459736);
     println!("part 2 = {}", horizontal * vertical);
 }
